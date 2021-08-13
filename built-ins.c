9380d3d593bbbc85e50e95e086d34557ae3b9e16
Precious-B <solomonprecious04@gmail.com>
2021-08-13 13:19:02 +0000

    Added more functions

---
+++ built-ins.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * is_builtin - This program checks to see if a command is
+ * a builtin function and if so, executes it
+ * @cmd: a vector array of command line arguments
+ * @b: line_buffer created in main
+ *
+ * Return: 1 if command is a builtin, 0 otherwise
+ */
+int is_builtin(char **cmd, char *b)
+{
+
+}
+
+/**
+ * env_builtin - This program is prints the current
+ * environment
+ *
+ * Return: 0
+ */
+void env_builtin(void)
+{
+
+}
